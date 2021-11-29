18bec670be184da8bd4d6b98b764ede15b626fba
amitkumar <uic.16mca8227@gmail.com>
2021-11-28 21:37:07 -0500

    'removing thr product from cart'

--- lib/Screens/cart_screen.dart
+++ lib/Screens/cart_screen.dart
@@ -42,6 +42,7 @@ class CartScreen extends StatelessWidget {
             itemBuilder: (ctx, i) {
               return CartItem(
                   cart.items.values.toList()[i]!.id,
+                  cart.items.keys.toList()[i],
                   cart.items.values.toList()[i]!.price,
                   cart.items.values.toList()[i]!.title,
                   cart.items.values.toList()[i]!.quanity);
--- lib/widget/cart_item.dart
+++ lib/widget/cart_item.dart
@@ -1,26 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String? id;
+  final String productId;
   final String? title;
   final double? price;
   final int? quantity;
 
-  CartItem(this.id, this.price, this.title, this.quantity);
+  CartItem(this.id,this.productId, this.price, this.title, this.quantity);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Card(
-        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: FittedBox(child: Text('\$$price')),
+      child: Dismissible(
+        key: ValueKey(id),
+        background: Container(
+          color: Colors.red,
+          child: const Icon(Icons.delete, color: Colors.white,size: 40,),
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.only(right: 20),
+
+        ),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction){
+          Provider.of<Cart>(context, listen: false).removeItem(productId);
+        },
+        child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: FittedBox(child: Text('\$$price')),
+            ),
+            title: Text(title!),
+            subtitle: Text('total: \$${price! * quantity!}'),
+            trailing: Text('$quantity x'),
           ),
-          title: Text(title!),
-          subtitle: Text('total: \$${price! * quantity!}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );
